f42a471c2cb735b3e6985cce559a508eb547cb16
Desiree3583 <72775978+Desiree3583@users.noreply.github.com>
2020-11-18 17:54:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-
+ while True:
+     print("Temperature (F) :" (input.temperature(Temperature.FAHRENHEIT) + ("Temperature (C) :" (input.temperature(Temperature.CELSIUS) 
+     print("Current Room Temperature: ("Temperature (F) :" (input.temperature(Temperature.FAHRENHEIT)) +("Temperature (C) :" (input.temperature(Temperature.CELSIUS)) 
